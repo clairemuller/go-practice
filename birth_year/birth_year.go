16b13fc5c529dbc98f763f51801535afecf6f82f
Claire Muller <clairermuller@gmail.com>
2019-10-10 12:19:42 -0700

    add comments and more messing around

--- birth_year/birth_year.go
+++ birth_year/birth_year.go
@@ -5,19 +5,32 @@ import (
 	"time"
 )
 
+// https://golangbot.com/structs/
+// creates named struct
+// versus anonymous struct, which would be assigned to a variable
 type Person struct {
 	name string
 	age  int
 }
 
-func (p Person) yearBorn(age int) int {
-	year := time.Now().Year()
-	birthYear := year - age
+// https://golangbot.com/methods/
+// creates a method, which is just a function
+// with a receiver argument btwn the func keyword and the method name
+func (p Person) yearBorn() int {
+	currYear := time.Now().Year()
+	birthYear := currYear - p.age
 	return birthYear
 }
 
 func main() {
-	p := Person{"Claire", 28}
-	result := p.yearBorn(p.age)
-	fmt.Println(result)
+	p1 := Person{"Claire", 28}
+
+	fmt.Printf("%s is %d and was born in %d\n", p1.name, p1.age, p1.yearBorn())
+	
+	p2 := Person{
+		name: "Kristen",
+		age:  36,
+	}
+
+	fmt.Printf("%s is %d and was born in %d\n", p2.name, p2.age, p2.yearBorn())
 }
