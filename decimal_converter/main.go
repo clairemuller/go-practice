ce36a78383532f1e03ff36ebd609d4e3ebbd9dae
clairemuller <clairermuller@gmail.com>
2019-10-21 14:08:30 -0700

    working on it

--- decimal_converter/main.go
+++ decimal_converter/main.go
@@ -3,31 +3,64 @@ package main
 import (
 	"fmt"
 	"math"
+	"strconv"
 )
 
-func toBinary(num float64) float64 {
-	// find highest power of 2 that is less than num
-	// start with 2 power of 0
-	// var power float64 = 0
-
-	// while num is less than or equal to 2^power, increase power
-	for num >= math.Pow(2, power) {
-		// fmt.Printf("2^%v = %v\n", power, math.Pow(2, power))
+func findPower(num float64, base int) int {
+	var power float64
+	b := float64(base)
+	for num >= math.Pow(b, power) {
 		power += 1
 	}
-
-
-	// var spaces
-
-	return power
+	return int(power - 1)
 }
 
-func toHex(num float64) float64 {
+func toBinary(num float64) int {
+	if num == 0 {
+		return 0
+	}
+	var binaryNum int
+	var binarySlice []int
+	firstLoop := true
+	var position int
+
+	for num >= 0 {
+		var power = findPower(num, 2)
+		if firstLoop {
+			// set the length of the binary number by adding zeroes
+			zeroes := power
+			binarySlice = append(binarySlice, 1)
+			for zeroes > 0 {
+				binarySlice = append(binarySlice, 0)
+				zeroes--
+			}
+			firstLoop = false
+		} else {
+			position = len(binarySlice)
+			position -= int(power)
+			position--
+			binarySlice[position] = 1
+		}
+		num -= math.Pow(2.0, float64(power))
+	}
+
+	// convert binarySlice into binaryNum
+	// binaryNum = int(binarySlice)
+	var binaryString string
+	for i := 0; i < len(binarySlice); i++ {
+		binaryString += strconv.Itoa(binarySlice[i])
+	}
+	fmt.Println(binaryString)
+	// fmt.Println(binaryNum)
+	return binaryNum
+}
+
+func toHex(num int) int {
 	return num + 100
 }
 
 func main() {
-	var num float64 = 34
+	var num float64 = 19
 	fmt.Println(toBinary(num))
 
 	// fmt.Printf("%v converted to binary is %v\n", num, toBinary(num))
