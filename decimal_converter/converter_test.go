9d345ed8617188dd25edeb9e988a373b9b2a013d
Claire Muller <clairermuller@gmail.com>
2019-10-22 14:17:53 -0700

    figure out decimal to hex

--- decimal_converter/converter_test.go
+++ decimal_converter/converter_test.go
@@ -27,3 +27,35 @@ func TestFormatBinary(t *testing.T) {
 		}
 	}
 }
+
+func TestFormatHex(t *testing.T) {
+	tables := []struct {
+		decimal float64
+		hex     string
+	}{
+		{-9, "Whole numbers only!"},
+		{7.4, "Whole numbers only!"},
+		{-83, "Whole numbers only!"},
+		{8623.5435, "Whole numbers only!"},
+		{0, "0"},
+		{7, "7"},
+		{11, "B"},
+		{18, "12"},
+		{39, "27"},
+		{63, "3F"},
+		{99, "63"},
+		{176, "B0"},
+		{335, "14F"},
+		{738, "2E2"},
+		{4095, "FFF"},
+		{66654, "1045E"},
+		{91757843, "5781D13"},
+	}
+
+	for _, table := range tables {
+		hexNum := FormatHex(table.decimal)
+		if hexNum != table.hex {
+			t.Errorf("FormatHex was incorrect, given: %v, got: %v, want: %v", table.decimal, hexNum, table.hex)
+		}
+	}
+}
--- decimal_converter/main.go
+++ decimal_converter/main.go
@@ -4,8 +4,18 @@ import (
 	"fmt"
 	"math"
 	"strconv"
+	"strings"
 )
 
+// check that num is greater than zero and not a decimal
+func wholeNumCheck(num float64) bool {
+	if num < 0 || num != math.Trunc(num) {
+		return false
+	}
+	return true
+}
+
+// find the highest power of num that is less than num
 func findPower(num float64, base float64) float64 {
 	var power float64
 	for num >= math.Pow(base, power) {
@@ -15,14 +25,13 @@ func findPower(num float64, base float64) float64 {
 }
 
 func FormatBinary(num float64) string {
-	// check that num is greater than zero and not a decimal
-	if num < 0 || num != math.Trunc(num) {
+	if !wholeNumCheck(num) {
 		return "Whole numbers only!"
 	}
 	if num == 0 {
 		return "0"
 	}
-	var binarySlice []int
+	var binarySlice []string
 	firstLoop := true
 	var power = findPower(num, 2)
 
@@ -32,30 +41,65 @@ func FormatBinary(num float64) string {
 		// otherwise add a 0
 		// decrement power each time
 		if firstLoop || num >= math.Pow(2.0, power) {
-			binarySlice = append(binarySlice, 1)
+			binarySlice = append(binarySlice, "1")
 			num -= math.Pow(2.0, power)
 			firstLoop = false
 		} else {
-			binarySlice = append(binarySlice, 0)
+			binarySlice = append(binarySlice, "0")
 		}
 		power--
 	}
-	// convert slice of numbers into a string
-	var binaryString string
-	for i := 0; i < len(binarySlice); i++ {
-		binaryString += strconv.Itoa(binarySlice[i])
-	}
-	return binaryString
+	// convert slice of strings into single string
+	return strings.Join(binarySlice, "")
 }
 
-func toHex(num int) int {
-	return num + 100
+func FormatHex(num float64) string {
+	if !wholeNumCheck(num) {
+		return "Whole numbers only!"
+	}
+	if num == 0 {
+		return "0"
+	}
+
+	var hexSlice []string
+	var power = findPower(num, 16)
+
+	for num >= 0 && power >= 0 {
+		// as long as num is greater/equal to 16^power
+		// decrement num by 16^power
+		// use count to keep track of how many times we decrement
+		count := 0
+		for num >= math.Pow(16.0, power) {
+			num -= math.Pow(16.0, power)
+			count++
+		}
+		// use count to determine what digit/char to add to slice
+		if count < 10 {
+			hexSlice = append(hexSlice, strconv.Itoa(count))
+		} else if count == 10 {
+			hexSlice = append(hexSlice, "A")
+		} else if count == 11 {
+			hexSlice = append(hexSlice, "B")
+		} else if count == 12 {
+			hexSlice = append(hexSlice, "C")
+		} else if count == 13 {
+			hexSlice = append(hexSlice, "D")
+		} else if count == 14 {
+			hexSlice = append(hexSlice, "E")
+		} else if count == 15 {
+			hexSlice = append(hexSlice, "F")
+		}
+		power--
+	}
+
+	return strings.Join(hexSlice, "")
 }
 
 func main() {
-	var num float64 = 9
-	fmt.Println(FormatBinary(num))
+	var num float64 = 10000
+	// fmt.Println(FormatBinary(num))
+	fmt.Println(FormatHex(num))
 
 	// fmt.Printf("%v converted to binary is %v\n", num, FormatBinary(num))
-	// fmt.Printf("%v converted to hexadecimal is %v\n", num, toHex(num))
+	// fmt.Printf("%v converted to hexadecimal is %v\n", num, FormatHex(num))
 }
